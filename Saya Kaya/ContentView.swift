d7bdcfb20cf66c0f8ee31eec0d71ec7b9660ada0
MilhamAt <M477ashiddiq@gmail.com>
2020-11-01 23:22:10 +0700

    Initial Commit

---
+++ Saya Kaya/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Saya Kaya
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 01/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Saya Kaya/Saya_KayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saya_KayaApp.swift
+//  Saya Kaya
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 01/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Saya_KayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
